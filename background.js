6768677b5fab8fb9c43e8c750e8e21718cc1b69c
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-03-11 19:53:57 +0900

    docsを追加

--- background.js
+++ background.js
@@ -6,3 +6,19 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.tabs.update(sender.tab.id, { muted: false });
   }
 });
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.tabs.create({ url: "https://anti-rickroll.commonjs.work" });
+  }
+
+  if (details.reason === "update") {
+    chrome.tabs.create({ url: "https://anti-rickroll.commonjs.work" });
+  }
+
+  chrome.storage.local.get({ ignoreList: [] }, (data) => {
+    if (!data.ignoreList) {
+      chrome.storage.local.set({ ignoreList: [] });
+    }
+  });
+});
